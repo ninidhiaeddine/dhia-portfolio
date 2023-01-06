90918d6f4fd6e551ffac504e7fd43014c5c464dc
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-06 15:10:14 +0200

    Deleted unnecessary font download

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import React from 'react';
 import Grid from "@mui/material/Grid";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Ubuntu from './Fonts/Ubuntu/Ubuntu-Regular.ttf';
 
 const router = createBrowserRouter([
   {
@@ -59,7 +60,21 @@ let darkTheme = createTheme({
       main: '#7C4DFF',
     },
     typography: {
-      fontFamily: ['Ubuntu'],
+      fontFamily: 'Ubuntu, Arial',
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: 'Ubuntu';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;
+            src: local('Ubuntu'), local('Ubuntu-Regular'), url(${Ubuntu}) format('ttf');
+            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+          }
+        `,
+      },
     },
   },
 });
@@ -67,7 +82,7 @@ let darkTheme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
+      <CssBaseline />
 
       <BrowserRouter><TopBar /></BrowserRouter>
 
