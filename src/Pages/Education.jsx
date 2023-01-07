397201765a3f4a0bb36d3474cbfb10a93043459f
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-07 22:25:03 +0200

    Added missing details in Education

--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -1,7 +1,7 @@
 import Stack from "@mui/material/Stack";
 import EducationCard from "../Components/EducationCard";
-import AuLogo from './Images/au logo.png';
-import AubLogo from './Images/Aub logo.png';
+import AuLogo from './Images/AuLogo.png';
+import AubLogo from './Images/AubLogo.png';
 
 export default function Education() {
     return (
@@ -13,14 +13,14 @@ export default function Education() {
                         title="American University of Beirut"
                         timeRangeString="2019 - 2023"
                         locationString="Beirut, Lebanon"
-                        descriptions={['BSc in Computer Science', 'Minor in Music']}
+                        descriptions={['BSc in Computer Science', 'Minor in Game Programming', 'Minor in Music']}
                         imgSrc={AubLogo} />
 
                     <EducationCard
                         title="American University"
-                        timeRangeString="Fall 2019"
+                        timeRangeString="Fall 2021"
                         locationString="Washington, D.C, USA"
-                        descriptions={['BSc in Computer Science', 'Minor in Music']}
+                        descriptions={['Exchange Semester (BSc in Computer Science)']}
                         imgSrc={AuLogo} />
                 </Stack></div>
 
