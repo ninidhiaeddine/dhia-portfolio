d6369192d7587388afc3376984d6d80b616dd498
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-07 22:24:45 +0200

    Added missing parts in art section

--- src/Pages/Art.jsx
+++ src/Pages/Art.jsx
@@ -1,7 +1,11 @@
-import SoloPic from './Images/SoloPic.jpg';
-import GroupPic from './Images/GroupPic.jpg';
+import SoloPic from './Images/ArtSoloPic.jpg';
+import GroupPic from './Images/ArtGroupPic.jpg';
+import { useNavigate } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
 
 export default function Art() {
+    const navigate = useNavigate();
+
     return (
         <>
             <div className="text-primary font-bold text-5xl pt-8 text-center">Art</div>
@@ -15,21 +19,32 @@ export default function Art() {
             animate-pulse"></img>
 
             <div className='relative top-[35rem]'>
-                <div className='bg-secondary '>
-                    <div className='text-2xl text-center text-tertiary text-bold p-4'>Chamber Music Performance</div>
+                <div className='bg-secondary pb-10'>
+                    <div className='text-2xl text-center text-tertiary font-bold p-4'>Chamber Music Performance</div>
                     <div className='flex justify-center'>
                         <iframe width="700" height="394" src="https://www.youtube.com/embed/RNaKVZNk5-c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
 
-                <div>
-                    <div className='text-2xl text-center text-white text-bold p-4'>Piano Covers</div>
-                </div>
-
-                <div className='bg-secondary '>
-                    <div className='text-2xl text-center text-tertiary text-bold p-4'>Shoft Films</div>
+                <div className='mt-5'>
+                    <div className='text-2xl text-center text-tertiary font-bold p-4'>Shoft Films</div>
                     <div className='flex justify-center'>
                         <iframe width="700" height="394" src="https://www.youtube.com/embed/No9A6AG1ym4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div className='bg-secondary mt-10 pb-5'>
+                    <div className='text-2xl text-center text-tertiary font-bold p-4'>Piano Covers</div>
+                    <div className='flex flex-col items-center'>
+                        You can find them on my Instagram Reels section!
+                        <a href="https://www.instagram.com/dhia.eddine.n/reels/" target="_blank" rel="noreferrer">
+                            <button className="w-[15rem] mt-5 bg-tertiary text-black py-2 px-4 border border-tertiary
+            hover:text-tertiary hover:border-tertiary hover:bg-background rounded hover:scale-110
+            transition ease-in-out"
+                            >
+                                Watch on my IG profile!
+                            </button>
+                        </a>
                     </div>
                 </div>
             </div>
