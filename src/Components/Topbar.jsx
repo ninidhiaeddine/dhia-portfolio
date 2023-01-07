c51c7e3c858be44959406cf3e76dc5aacef7737d
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-07 22:24:34 +0200

    Fixed issue in Top bar not redirecting to home

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -5,7 +5,10 @@ export default function TopBar() {
     let navigate = useNavigate();
 
     return (
-        <div className="flex justify-center items-center h-fit mt-3" onClick={() => navigate('/')}>
+        <div className="flex justify-center items-center h-fit mt-3" onClick={() => {
+            navigate('/');
+            navigate(0);
+        }}>
             <div className="flex transition hover:scale-110 duration-300 ease-in-out animate-pulse">
                 <div className="text-white text-2xl"><b>Dhia Eddine</b> Nini</div>
                 <div className="font-bold text-primary text-2xl">.</div>
