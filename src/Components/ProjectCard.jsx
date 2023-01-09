8239a19a95e8996bed753d613fb9a4924c5f9e1e
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-09 02:58:55 +0200

    Further improvement to card look

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -4,29 +4,43 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
 
-export default function ProjectCard({ title, imgSrc, description }) {
+export default function ProjectCard({ title, imgSrc, skills, description, link }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card>
       <CardMedia
-        sx={{ height: 140 }}
+        sx={{ height: 200 }}
         image={imgSrc}
-        title="Title"
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent sx={{paddingTop: 3, paddingLeft: 3, paddingRight: 3}} >
+        <Typography gutterBottom variant="h5" component="div" className='h-16'>
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" className='text-justify h-[8rem]' >
           {description}
         </Typography>
+        <Grid container spacing={1} sx={{textAlign: 'center', marginTop: 2, height: 50}}>
+          {
+            skills.map(x => {
+              return <Grid item xs={4}>
+                <Chip label={x} variant="outlined" />
+              </Grid>
+            })
+          }
+        </Grid>
       </CardContent>
-      <CardActions>
-        <button className='bg-background text-primary py-1 px-3 border border-primary rounded 
+      <CardActions sx={{padding: 2, justifyContent: 'end'}}>
+        <a href={link}
+          target="_blank"
+          rel="noopener noreferrer">
+          <button className='bg-background text-primary py-1 px-3 border border-primary rounded 
         hover:bg-primary hover:text-white hover:border-transparent hover:scale-110
         transition ease-in-out'>
-          View Source Code
-        </button>
+            View Source Code
+          </button>
+        </a>
       </CardActions>
     </Card>
   );
