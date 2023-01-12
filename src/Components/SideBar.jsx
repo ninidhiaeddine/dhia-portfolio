cc172c78835c25aa902405b63cb62d1e9125482f
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-12 13:10:35 +0200

    Fixed mobile nav bar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -18,7 +18,8 @@ import ChordAudio from '../Audio/mixkit-chord-swell-short-692.wav';
 
 export default function SideBar() {
     return (
-        <div className="z-100 fixed flex flex-row bottom-0 bg-background md:bg-transparent md:flex-col md:top-12 md:left-0">
+        <div className="z-50 fixed flex flex-row bottom-0 bg-background shadow-lg border border-white rounded-full
+        md:border-none md:bg-transparent md:flex-col md:top-12 md:left-0">
             <SideBarItem Icon={HomeIcon} text="Home" linkTo='/' audio={ChordAudio} />
             <SideBarItem Icon={BuildIcon} text="Skills" linkTo='Skills' audio={ChordAudio} />
             <SideBarItem Icon={SchoolIcon} text="Education" linkTo='Education' audio={ChordAudio} />
--- src/Components/SideBarItem.jsx
+++ src/Components/SideBarItem.jsx
@@ -11,12 +11,12 @@ export default function SideBarItem({Icon, text, linkTo, audio}) {
     }
 
     return (
-        <div className={`flex items-center border border-white text-white rounded-full text-center 
+        <div className={`flex items-center md:border md:border-white text-white rounded-full text-center 
             w-10 h-10 m-2 md:m-2 md:w-14 md:h-14
-            hover:w-36 hover:scale-105 hover:bg-primary hover:border-none
+            md:hover:w-36 md:hover:scale-105 md:hover:bg-primary md:hover:border-none
             transition-all duration-200
             animate-pulse hover:animate-none
-            z-[100]`}
+            z-50`}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             onClick={() => {
